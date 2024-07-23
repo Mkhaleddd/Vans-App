4b1463c764f26e37160631edb855c38fe4bdd1b6
mariem <mariamkhaled709@gmail.com>
2024-07-23 21:22:10 +0300

    Updates

---
+++ assets/SignUp-I8KQVJfQ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fyX-0t_3.js";import{S as e}from"./SinglePageLog-WxPsCh6c.js";function o(){return t.jsx(e,{title:"Create an Account",login:!1,description:"Have an Account already?",navText:"Log In"})}export{o as default};
